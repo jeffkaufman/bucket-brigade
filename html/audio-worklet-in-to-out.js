282ead7a45fd22291480571357141633331060c0
Glenn Willen <gwillen@nerdnet.org>
2020-07-05 01:36:13 -0700

    better underflow handling (good) and binary protocol + Float32Array stuff (not as clear)

--- html/audio-worklet-in-to-out.js
+++ html/audio-worklet-in-to-out.js
@@ -3,35 +3,56 @@ const FRAME_SIZE = 128;  // by Web Audio API spec
 class Player extends AudioWorkletProcessor {
   constructor () {
     super();
+    this.min_buffer_size = 1;  // in chunks of SAMPLE_BATCH_SIZE frames of 128 samples each
+    this.max_buffer_size = 2
     this.queue = [];
-    this.min_buffer_size = 2;  // in frames of 128 samples / about 3ms
+    this.offset = 0;
+    this.underflow_skip = 0;
     this.started = false;
     this.debug_ctr = 0;
     this.port.onmessage = (event) => {
       if (this.debug_ctr % 10 == 0) {
-        console.log("audio buffer length (samples): ", this.queue.length, ", new input (samples): ", event.data.length);
+        console.log("audio buffer length (chunks of SAMPLE_BATCH_SIZE frames of 128 samples each): ", this.queue.length, ", new input (samples): ", event.data.length);
       }
       this.debug_ctr++;
-      this.queue = this.queue.concat(event.data);
+      if (this.queue.length == this.max_buffer_size) {
+        console.log("OVERFLOW");
+        return;
+      }
+      this.queue.push(event.data);
     }
   }
+
   process (inputs, outputs, parameters) {
-    this.port.postMessage(Array.from(inputs[0][0]));
+    // Transfer input data (don't copy) to main thread.
+    this.port.postMessage(inputs[0][0], [inputs[0][0].buffer]);
     // Buffer a bit before we get started.
-    if (this.queue.length < FRAME_SIZE * this.min_buffer_size && !this.started) {
+    if (this.queue.length < this.min_buffer_size && !this.started) {
       return true;
     }
     this.started = true;
-    if (this.queue < FRAME_SIZE) {
-      console.log("UNDERFLOW");
-      return true; // hack
-    }
-    var samples = this.queue.slice(0, FRAME_SIZE);
-    this.queue = this.queue.slice(FRAME_SIZE);
-    for (var chan = 0; chan < outputs[0].length; chan++) {
-      for (var i = 0; i < samples.length; i++) {
-        outputs[0][chan][i] = samples[i];
+    while (this.queue.length > 0 && this.underflow_skip > 0) {
+      console.log("skip to compensate underflow");
+      this.underflow_skip--;
+      this.offset += FRAME_SIZE;
+      if (this.offset == this.queue[0].length) {
+        this.offset = 0;
+        this.queue = this.queue.slice(1);
       }
+    }
+    if (this.queue.length == 0) {
+      console.log("UNDERFLOW");
+      this.underflow_skip++;
+      this.started = false;  // Build back up to minimum buffer
+      return true;
+    }
+    for (var chan = 0; chan < outputs[0].length; chan++) {
+      outputs[0][chan].set(this.queue[0].slice(this.offset, this.offset +  FRAME_SIZE));
+    }
+    this.offset += FRAME_SIZE;
+    if (this.offset == this.queue[0].length) {
+      this.offset = 0;
+      this.queue = this.queue.slice(1);
     }
     return true;
   }
--- server.py
+++ server.py
@@ -3,19 +3,24 @@
 import http.server
 from http.server import BaseHTTPRequestHandler
 import json
-from collections import deque
 
 N_LAYERS = 5
 
 # TODO: Write pointer trails read pointer to offset round-trip latency
-queue = [0] * (10 * 44100)
+queue = bytearray(10 * 44100)
 rdptr = [0] * N_LAYERS
 wrptr = [0] * N_LAYERS
 
 class OurHandler(BaseHTTPRequestHandler):
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.end_headers()
+
     def do_POST(self):
         content_length = int(self.headers["Content-Length"])
-        in_data = json.loads(self.rfile.read(content_length))
+        in_data = self.rfile.read(content_length)
         layer = int(self.path[1:])
 
         for i in range(len(in_data)):
@@ -24,15 +29,16 @@ class OurHandler(BaseHTTPRequestHandler):
 
         self.send_response(200)
         self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
         self.end_headers()
 
-        data = [0] * len(in_data)
+        out_data = bytearray(len(in_data))
         if (layer != 1):
             for i in range(len(in_data)):
-                data[i] = queue[rdptr[layer]]
+                out_data[i] = queue[rdptr[layer]]
                 rdptr[layer] = (rdptr[layer] + 1) % len(queue)
 
-        self.wfile.write(json.dumps(data).encode("UTF-8"))
+        self.wfile.write(out_data)
 
 server = http.server.HTTPServer(('', 8081), OurHandler)
 server.serve_forever()
