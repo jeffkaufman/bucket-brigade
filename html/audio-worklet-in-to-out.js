21142824b58ce340981b24ad62d6d469fac079bf
Glenn Willen <gwillen@nerdnet.org>
2020-07-04 19:40:33 -0700

    change stream framing; beginning of layered server (end of pairing)

--- html/audio-worklet-in-to-out.js
+++ html/audio-worklet-in-to-out.js
@@ -4,6 +4,7 @@ class Player extends AudioWorkletProcessor {
   constructor () {
     super();
     this.queue = [];
+    this.min_buffer_size = 100;
     this.started = false;
     this.debug_ctr = 0;
     this.port.onmessage = (event) => {
@@ -15,16 +16,18 @@ class Player extends AudioWorkletProcessor {
     }
   }
   process (inputs, outputs, parameters) {
+    this.frame_size = inputs[0][0].length;
     this.port.postMessage(Array.from(inputs[0][0]));
-    if (this.queue.length < 100 && !this.started) {
+    if (this.queue.length < this.frame_size * this.min_buffer_size && !this.started) {
       return true;
     }
     this.started = true;
-    var samples = this.queue.shift();
-    if (!samples) {
+    if (this.queue < this.frame_size) {
       console.log("UNDERFLOW");
       return true; // hack
     }
+    var samples = this.queue.slice(0, this.frame_size);
+    this.queue = this.queue.slice(this.frame_size);
     for (var chan = 0; chan < outputs[0].length; chan++) {
       for (var i = 0; i < samples.length; i++) {
         outputs[0][chan][i] = samples[i];
--- server.py
+++ server.py
@@ -2,16 +2,37 @@
 
 import http.server
 from http.server import BaseHTTPRequestHandler
+import json
+from collections import deque
+
+N_LAYERS = 5
+
+# TODO: Write pointer trails read pointer to offset round-trip latency
+queue = [0] * (10 * 44100)
+rdptr = [0] * N_LAYERS
+wrptr = [0] * N_LAYERS
 
 class OurHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers["Content-Length"])
-        data = self.rfile.read(content_length)
-        print(data)
+        in_data = json.loads(self.rfile.read(content_length))
+        layer = int(self.path[1:])
+
+        for i in range(len(in_data)):
+            queue[wrptr[layer]] = in_data[i]
+            wrptr[layer] = (wrptr[layer] + 1) % len(queue)
+
         self.send_response(200)
         self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
-        self.wfile.write(data)
+
+        data = [0] * len(in_data)
+        if (layer != 1):
+            for i in range(len(in_data)):
+                data[i] = queue[rdptr[layer]]
+                rdptr[layer] = (rdptr[layer] + 1) % len(queue)
+
+        self.wfile.write(json.dumps(data).encode("UTF-8"))
 
 server = http.server.HTTPServer(('', 8081), OurHandler)
 server.serve_forever()
