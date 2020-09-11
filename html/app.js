33a13992846800a3d5479349c90dbf67efd7165a
Glenn Willen <gwillen@nerdnet.org>
2020-09-10 17:43:26 -0700

    move networking to net.js

--- html/app.js
+++ html/app.js
@@ -1,6 +1,7 @@
 import * as lib from './lib.js';
 import {LOG_VERYSPAM, LOG_SPAM, LOG_DEBUG, LOG_INFO, LOG_WARNING, LOG_ERROR} from './lib.js';
 import {LOG_LEVELS} from './lib.js';
+import {query_server_clock, samples_to_server} from './net.js';
 
 // Work around some issues related to caching and error reporting
 //   by forcing this to load up top, before we try to 'addModule' it.
@@ -358,7 +359,6 @@ var mic_buf_clock;
 var loopback_mode;
 var server_path;
 var audio_offset;
-var xhrs_inflight;
 var override_gain = 1.0;
 var synthetic_audio_source;
 var synthetic_audio_sink;
@@ -379,35 +379,6 @@ function batches_to_ms(batches) {
 // In units of 128 samples.
 var sample_batch_size = null;  // set by start()
 var max_sample_batch_size = ms_to_batches(MAX_MS_PER_BATCH);
-
-async function query_server_clock() {
-  if (loopback_mode == "main") {
-    // I got yer server right here!
-    server_clock = Math.round(Date.now() * sample_rate / 1000.0);
-  } else {
-    // Support relative paths
-    var target_url = new URL(server_path, document.location);
-    var request_time_ms = Date.now();
-    var fetch_result = await fetch(target_url, {
-      method: "get",  // default
-      cache: "no-store",
-    });
-    // We need one-way latency; dividing by 2 is unprincipled but probably close enough.
-    // XXX: This is not actually correct. We should really be using the roundtrip latency here. Because we want to know not "what is the server clock now", but "what will the server clock be by the time my request reaches the server."
-    // Proposed alternative:
-    /*
-      var request_time_samples = Math.round(request_time_ms * sample_rate / 1000.0);
-      var metadata = JSON.parse(fetch_result.headers.get("X-Audio-Metadata"));
-      // Add this to "our time now" to yield "server time when it gets our request."
-      server_sample_offset = metadata["server_clock"] - request_time_samples;
-      // Note: In the presence of network jitter, our message can get to the server either before or after the target server moment. This means that if our target server moment is "now", our actual requested moment could end up in the future. Someone on one side or the other has to deal with this. But in general if we are requesting "now" it means we do not expect to get audio data at all, so it should be okay for us to never ask for audio data in the case (and it should be ok for the server to give us zeros for "future" data, since we should never have asked, but that's what _would_ be there.)
-    */
-    var server_latency_ms = (Date.now() - request_time_ms) / 2.0;  // Wrong, see above
-    var metadata = JSON.parse(fetch_result.headers.get("X-Audio-Metadata"));
-    server_clock = Math.round(metadata["server_clock"] + server_latency_ms * sample_rate / 1000.0);
-    lib.log(LOG_INFO, "Server clock is estimated to be:", server_clock, " (", metadata["server_clock"], "+", server_latency_ms * sample_rate / 1000.0);
-  }
-}
 
 function set_estimate_latency_mode(mode) {
   playerNode.port.postMessage({
@@ -587,7 +558,15 @@ async function start() {
 
   window.clapAlong.style.display = "block";
 
-  set_estimate_latency_mode(true);
+  // XXX
+  //set_estimate_latency_mode(true);
+  window.est40to60.innerText = "0ms";
+  window.estLatency.innerText = "0ms";
+  send_local_latency();
+  window.clapAlong.style.display = "none";
+  window.runningInstructions.style.display = "block";
+  //set_estimate_latency_mode(false);
+  // XXX
 }
 
 async function reload_settings() {
@@ -596,13 +575,14 @@ async function reload_settings() {
   read_clock = null;
   mic_buf = [];
   mic_buf_clock = null;
-  xhrs_inflight = 0;
   override_gain = 1.0;
 
   loopback_mode = loopback_mode_select.value;
   server_path = server_path_text.value;
 
-  await query_server_clock();
+  // Support relative paths
+  var target_url = new URL(server_path, document.location);
+  server_clock = await query_server_clock(loopback_mode, target_url, sample_rate);
   read_clock = server_clock - audio_offset;
 
   // Send this before we set audio params, which declares us to be ready for audio
@@ -702,12 +682,21 @@ function handle_message(event) {
 
   //XXX lib.log_every(10, "audioCtx", LOG_DEBUG, "audioCtx:", audioCtx);
   var mic_samples = msg.samples;
+  var send_write_clock = msg.clock;
   mic_buf.push(mic_samples);
 
+  // Update the peak meter
   var peak_in = parseFloat(peak_in_text.value);
   if (isNaN(peak_in)) {
     peak_in = 0.0;
   }
+  for (var i = 0; i < mic_samples.length; i++) {
+    if (Math.abs(mic_samples[i]) > peak_in) {
+      peak_in = Math.abs(mic_samples[i]);
+    }
+  }
+  // XXX: touching the DOM
+  peak_in_text.value = peak_in;
 
   if (mic_buf.length >= sample_batch_size) {
     // Resampling here works automatically for now since we're copying sample-by-sample
@@ -715,26 +704,16 @@ function handle_message(event) {
     if (msg.clock !== null) {
       for (var i = 0; i < mic_buf.length; i++) {
         for (var j = 0; j < mic_buf[i].length; j++) {
-          if (Math.abs(mic_buf[i][j]) > peak_in) {
-            peak_in = Math.abs(mic_buf[i][j]);
-          }
           outdata[i * mic_buf[0].length + j] = sample_encoding["send"](mic_buf[i][j]);
-        }
-      }
-      // XXX: touching the DOM
-      peak_in_text.value = peak_in;
-    } else {
-      // Still warming up
-      for (var i = 0; i < mic_buf.length; i++) {
-        for (var j = 0; j < mic_buf[i].length; j++) {
-          if (mic_buf[i][j] !== 0) {
-            lib.log_every(12800, "nonzero_warmup", LOG_ERROR, "Nonzero mic data during warmup, huh?");
-            //throw "Nonzero mic data during warmup, huh?"
-          }
         }
       }
     }
     mic_buf = [];
+
+    // Clocks are at the _end_ of intervals; the longer we've been
+    //   accumulating data, the more we have to read. (Trust me.)
+    var orig_read_clock = read_clock;
+    read_clock += outdata.length;
 
     if (loopback_mode == "main") {
       lib.log(LOG_DEBUG, "looping back samples in main thread");
@@ -742,68 +721,50 @@ function handle_message(event) {
       for (var i = 0; i < outdata.length; i++) {
         result[i] = sample_encoding["recv"](outdata[i]);
       }
-      // Clocks are at the _end_ of intervals; the longer we've been
-      //   accumulating data, the more we have to read. (Trust me.)
-      read_clock += outdata.length;
       samples_to_worklet(result, read_clock);
       return;
-    }
-
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState == 4 /* done*/) {
-        handle_xhr_result(xhr);
-      }
-    };
-    xhr.debug_id = Date.now();
-
-    try {
+    } else {
       var target_url = new URL(server_path, document.location);
-      var params = new URLSearchParams();
-      // Response size always equals request size.
-      // Clocks are at the _end_ of intervals; the longer we've been
-      //   accumulating data, the more we have to read. (Trust me.)
-      read_clock += outdata.length;
-      params.set('read_clock', read_clock);
-      if (msg.clock !== null) {
-        params.set('write_clock', msg.clock);
-      }
-      if (loopback_mode == "server") {
-        params.set('loopback', true);
-        lib.log(LOG_DEBUG, "looping back samples at server");
-      }
-      const username = window.userName.value;
-      if (username) {
-        params.set('username', username);
-      }
-      if (chatsToSend.length) {
-        params.set('chat', JSON.stringify(chatsToSend));
-        chatsToSend = [];
-      }
-      if (requestedLeadPosition) {
-        params.set('request_lead', '1');
-        requestedLeadPosition = false;
-      }
 
-      target_url.search = params.toString();
+      // This has a few too many arguments, but I think it's still an improvement to factor it out.
+      samples_to_server(outdata, orig_read_clock, send_write_clock, window.userName.value, chatsToSend, requestedLeadPosition, target_url, loopback_mode).then((response) => {
+        var result = new sample_encoding["client"](response.data);
+        var metadata = response.metadata;
 
-      // Arbitrary cap; browser cap is 8(?) after which they queue
-      if (xhrs_inflight >= 4) {
-        lib.log(LOG_WARNING, "NOT SENDING XHR w/ ID:", xhr.debug_id, " due to limit -- already in flight:", xhrs_inflight);
-        try_increase_batch_size_and_reload();
-        return
-      }
+        var play_samples = new Float32Array(result.length);
+        for (var i = 0; i < result.length; i++) {
+          play_samples[i] = sample_encoding["recv"](result[i]);
+          if (Math.abs(play_samples[i]) > peak_out) {
+            peak_out = Math.abs(play_samples[i]);
+          }
+        }
+        samples_to_worklet(play_samples, metadata["client_read_clock"]);
 
-      lib.log(LOG_SPAM, "Sending XHR w/ ID:", xhr.debug_id, "already in flight:", xhrs_inflight++, "; data size:", outdata.length);
-      xhr.open("POST", target_url, true);
-      xhr.setRequestHeader("Content-Type", "application/octet-stream");
-      xhr.responseType = "arraybuffer";
-      xhr.send(outdata);
-      lib.log(LOG_SPAM, "... XHR sent.");
-    } catch(e) {
-      lib.log(LOG_ERROR, "Failed to make XHR:", e);
-      stop();
-      return;
+        var queue_size = metadata["queue_size"];
+        var user_summary = metadata["user_summary"];
+        var chats = metadata["chats"];
+        var delay_samples = metadata["delay_samples"];
+
+        // Defer touching the DOM, just to be safe.
+        requestAnimationFrame(() => {
+          if (delay_samples) {
+            delay_samples = parseInt(delay_samples, 10);
+            if (delay_samples > 0) {
+              audio_offset_text.value = Math.round(delay_samples / sample_rate);
+              audio_offset_change();
+            }
+          }
+
+          update_active_users(user_summary);
+
+          chats.forEach((msg) => receiveChatMessage(msg[0], msg[1]));
+
+          peak_out_text.value = peak_out;
+
+          client_total_time.value = (metadata["client_read_clock"] - metadata["client_write_clock"] + play_samples.length) / sample_rate;
+          client_read_slippage.value = (metadata["server_clock"] - metadata["client_read_clock"] - audio_offset) / sample_rate;
+        });
+      });
     }
   }
 }
@@ -833,66 +794,6 @@ function update_active_users(user_summary) {
     tr.appendChild(td2);
 
     window.activeUsers.appendChild(tr);
-  }
-}
-
-
-// Only called when readystate is 4 (done)
-function handle_xhr_result(xhr) {
-  if (!running) {
-    lib.log(LOG_WARNING, "Got XHR onreadystatechange w/ID:", xhr.debug_id, "for xhr:", xhr, " when done running; still in flight:", --xhrs_inflight);
-    return;
-  }
-
-  if (xhr.status == 200) {
-    var metadata = JSON.parse(xhr.getResponseHeader("X-Audio-Metadata"));
-    lib.log(LOG_DEBUG, "metadata:", metadata);
-    var result = new sample_encoding["client"](xhr.response);
-    var play_samples = new Float32Array(result.length);
-    for (var i = 0; i < result.length; i++) {
-      play_samples[i] = sample_encoding["recv"](result[i]);
-      if (Math.abs(play_samples[i]) > peak_out) {
-        peak_out = Math.abs(play_samples[i]);
-      }
-    }
-
-    lib.log(LOG_SPAM, "Got XHR response w/ ID:", xhr.debug_id, "result:", result, " -- still in flight:", --xhrs_inflight);
-    if (metadata["kill_client"]) {
-      lib.log(LOG_ERROR, "Received kill from server");
-      reload_settings();
-      return;
-    }
-    samples_to_worklet(play_samples, metadata["client_read_clock"]);
-
-    var queue_size = metadata["queue_size"];
-    var user_summary = metadata["user_summary"];
-    var chats = metadata["chats"];
-    var delay_samples = metadata["delay_samples"];
-
-    // Defer touching the DOM, just to be safe.
-    requestAnimationFrame(() => {
-      if (delay_samples) {
-        delay_samples = parseInt(delay_samples, 10);
-        if (delay_samples > 0) {
-          audio_offset_text.value = Math.round(delay_samples / sample_rate);
-          audio_offset_change();
-        }
-      }
-
-      update_active_users(user_summary);
-
-      chats.forEach((msg) => receiveChatMessage(msg[0], msg[1]));
-
-      peak_out_text.value = peak_out;
-
-      client_total_time.value = (metadata["client_read_clock"] - metadata["client_write_clock"] + play_samples.length) / sample_rate;
-      client_read_slippage.value = (metadata["server_clock"] - metadata["client_read_clock"] - audio_offset) / sample_rate;
-    });
-  } else {
-    lib.log(LOG_ERROR, "XHR failed w/ ID:", xhr.debug_id, "stopping:", xhr, " -- still in flight:", --xhrs_inflight);
-
-    try_increase_batch_size_and_reload();
-    return;
   }
 }
 
