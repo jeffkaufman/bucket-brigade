79f954065a955bc1ed37d3267effe2e1eec23233
Jeff Kaufman <jeff.t.kaufman@gmail.com>
2020-09-27 01:13:47 +0000

    more debugging

--- server.py
+++ server.py
@@ -52,7 +52,7 @@ class User:
         self.opus_state = None
         self.mic_volume = 1.0
         self.scaled_mic_volume = 1.0
-        self.last_write_clock = None
+        self.last_write_clock = 0
         # For debugging purposes only
         self.last_seen_read_clock = None
         self.last_seen_write_clock = None
