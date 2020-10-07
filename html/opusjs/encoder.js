73ed5666705dac1a7390699623981e34d93dffbc
Jeff Kaufman <jeff.t.kaufman@gmail.com>
2020-10-07 21:29:30 +0000

    See what a higher bit rate sounds like

--- html/opusjs/encoder.js
+++ html/opusjs/encoder.js
@@ -326,7 +326,7 @@ var OpusEncoder = (function() {
       });
       return
     }
-    _opus_encoder_ctl(this.handle, /*OPUS_SET_BITRATE_REQUEST=*/4002, 65536);
+    _opus_encoder_ctl(this.handle, /*OPUS_SET_BITRATE_REQUEST=*/4002, 131072);
   });
   OpusEncoder.prototype.setup = (function(config) {
     var request_id = config.request_id;
@@ -373,7 +373,7 @@ var OpusEncoder = (function() {
       });
       return
     }
-    _opus_encoder_ctl(this.handle, /*OPUS_SET_BITRATE_REQUEST=*/4002, 65536);
+    _opus_encoder_ctl(this.handle, /*OPUS_SET_BITRATE_REQUEST=*/4002, 131072);
     if (this.sampling_rate != this.codec_sampling_rate) {
       try {
         this.resampler = new SpeexResampler(this.channels, this.sampling_rate, this.codec_sampling_rate)
