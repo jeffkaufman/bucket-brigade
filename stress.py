6eb584be19a4033da3ca8338cb702434bffd26b4
Jeff Kaufman <jeff.t.kaufman@gmail.com>
2020-11-03 16:57:21 +0000

    update stress tester

--- stress.py
+++ stress.py
@@ -1,25 +1,35 @@
 import sys
 import time
 import subprocess
+import opuslib
+import numpy as np
+import server
+import tempfile
 
-SAMPLE_RATE=11025
-
-def prepare():
-  with open("/tmp/stress.empty", "w") as outf:
-    for i in range(1000):
-      outf.write('\0')
+enc = opuslib.Encoder(
+  server.SAMPLE_RATE, server.CHANNELS, opuslib.APPLICATION_AUDIO)
+zeros = np.zeros(28800).reshape([-1, server.OPUS_FRAME_SAMPLES])
 
 def send_request():
-  ts = int(time.time()) * SAMPLE_RATE
-  cmd = [
-    'curl',
-    'https://echo.jefftk.com/api/?read_clock=%s' % ts,
-    '-H', 'Content-Type: application/octet-stream',
-    '--data-binary', "@/tmp/stress.empty",
-    '--compressed',
-    '-sS',
-    '-o', '/dev/null']
-  subprocess.check_call(cmd)
+  data = server.pack_multi([
+    np.frombuffer(
+      enc.encode_float(packet.tobytes(), server.OPUS_FRAME_SAMPLES),
+      np.uint8)
+    for packet in zeros]).tobytes()
+  ts = int(time.time()) * server.SAMPLE_RATE
+
+  with tempfile.NamedTemporaryFile() as tmp:
+    tmp.write(data)
+    tmp.flush()
+    cmd = [
+      'curl',
+      'https://echo.jefftk.com/api/?read_clock=%s&userid=1234&username=stress' % ts,
+      '-H', 'Content-Type: application/octet-stream',
+      '--data-binary', "@" + tmp.name,
+      '--compressed',
+      '-sS',
+      '-o', '/dev/null']
+    subprocess.check_call(cmd)
 
 def stress():
   while True:
@@ -30,5 +40,4 @@ def stress():
     print("elapsed: %sms" % int((end-start)*1000))
   
 if __name__ == "__main__":
-  prepare()
   stress()
