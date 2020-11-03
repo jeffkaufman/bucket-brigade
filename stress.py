df4432f79e922ace55440ffd05953c0f0994ce01
Jeff Kaufman <jeff@jefftk.com>
2020-11-03 12:41:00 -0500

    Heavy duty stress tester

--- stress.py
+++ stress.py
@@ -6,10 +6,16 @@ import numpy as np
 import server
 import tempfile
 import random
+from multiprocessing import Pool
+import time
+
+PACKET_INTERVAL = 0.6 # 600ms
+
 
 enc = opuslib.Encoder(
   server.SAMPLE_RATE, server.CHANNELS, opuslib.APPLICATION_AUDIO)
-zeros = np.zeros(28800).reshape([-1, server.OPUS_FRAME_SAMPLES])
+zeros = np.zeros(int(server.SAMPLE_RATE * PACKET_INTERVAL)).reshape(
+  [-1, server.OPUS_FRAME_SAMPLES])
 
 def send_request(tmp_name, userid):
   ts = int(time.time()) * server.SAMPLE_RATE
@@ -25,9 +31,36 @@ def send_request(tmp_name, userid):
     '-o', '/dev/null']
   subprocess.check_call(cmd)
 
-def stress():
+def summarize(timing):
+  return min(timing), max(timing), sum(timing)//len(timing)
+
+def stress(args):
+  # avoid having everyone at the same offset
+  time.sleep(random.random() * PACKET_INTERVAL)
+
+  n_rounds, tmp_name = args
+
   userid = int(random.random()*10000000)
-  
+  timing = []
+  for i in range(n_rounds):
+    start = time.time()
+    send_request(tmp_name, userid)
+    end = time.time()
+
+    duration = end-start
+    timing.append(int(duration*1000))
+
+    if duration < PACKET_INTERVAL:
+      time.sleep(PACKET_INTERVAL - duration)
+
+  return timing
+
+def run(n_workers, n_rounds):
+  n_workers = int(n_workers)
+  n_rounds = int(n_rounds)
+
+  pool = Pool(n_workers)
+
   with tempfile.NamedTemporaryFile() as tmp:
     data = server.pack_multi([
       np.frombuffer(
@@ -37,12 +70,11 @@ def stress():
     tmp.write(data)
     tmp.flush()
 
-    while True:
-      start = time.time()
-      send_request(tmp.name, userid)
-      end = time.time()
-
-      print("elapsed: %sms" % int((end-start)*1000))
+    timings = []
+    for timing in pool.map(stress, [
+        (n_rounds, tmp.name) for i in range(n_workers)]):
+      timings.extend(timing)
+    print ("[min=%s  max=%s  avg=%s]" % summarize(timings))
 
 if __name__ == "__main__":
-  stress()
+  run(*sys.argv[1:])
