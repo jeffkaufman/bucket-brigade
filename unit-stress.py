08e454f6fdd3609b43bc26c42772b6e16d2daa9c
Jeff Kaufman <jeff.t.kaufman@gmail.com>
2020-11-12 01:29:58 +0000

    revive unit stress
    I measure:
    
    $ python3 unit-stress.py
    8.05ms each; est 75 clients
    8.12ms each; est 74 clients
    7.78ms each; est 77 clients
    
    I suspect most of this time is opus, but I have not verified that yet.
    
    This is consistent with what I saw with full end to end stress testing, which could handle 50 clients but not 75. Of course, end to end testing has more overhead, but probably not much?

--- unit-stress.py
+++ unit-stress.py
@@ -3,22 +3,45 @@ import time
 import random
 import numpy as np
 import server
+import opuslib
 
-in_data_raw = np.random.default_rng().bytes(10*128)
+PACKET_INTERVAL = 0.6 # 600ms
+PACKET_SAMPLES = int(server.SAMPLE_RATE * PACKET_INTERVAL)
+
+enc = opuslib.Encoder(
+  server.SAMPLE_RATE, server.CHANNELS, opuslib.APPLICATION_AUDIO)
+zeros = np.zeros(PACKET_SAMPLES).reshape(
+  [-1, server.OPUS_FRAME_SAMPLES])
+
+data = server.pack_multi([
+  np.frombuffer(
+    enc.encode_float(packet.tobytes(), server.OPUS_FRAME_SAMPLES),
+    np.uint8)
+  for packet in zeros]).tobytes()
+
+userid = int(random.random()*10000000)
+username = "unitstress"
 
 def fake_request():
-  server.handle_post(in_data_raw, query_params = {
-    "read_clock": str(int(time.time()) * server.SAMPLE_RATE)
-  })
+  server.handle_post(data, query_params = {
+    "read_clock": (str(int(time.time()) * server.SAMPLE_RATE),),
+    "userid": (userid,),
+    "username": (username,),
+  }, environ=None)
 
 def stress():
-  while True:
+  for i in range(3):
     start = time.time()
-    for i in range(1000):
+    n_requests = 1000
+    for i in range(n_requests):
       fake_request()
     end = time.time()
 
-    print("elapsed: %sms" % int((end-start)*1000))
-  
+    each_s = (end-start)/n_requests
+
+    print("%.2fms each; est %.0f clients" % (
+      each_s*1000,
+      PACKET_INTERVAL/each_s))
+
 if __name__ == "__main__":
   stress()
