jeffkaufman/bucket-brigade

commit 6cca76cae807a9d7a8fa41d6230256a22a35d2f4

Add a bunch of telemetry; compensate for dropped process() calls instead of dying

server.py
@@ -16,6 +16,7 @@ import threading
 import datetime
 import struct
 import subprocess
+import copy
 
 from typing import Any, Dict, List, Tuple, Iterable
 
@@ -41,6 +42,8 @@ try:
 except Exception:
     SERVER_VERSION="unknown"
     SERVER_BRANCH="unknown"
+
+SERVER_STARTUP_TIME = int(time.time())
 
 class State():
     def __init__(self):
@@ -281,6 +284,10 @@ class User:
         # For debugging purposes only
         self.last_seen_read_clock = None
         self.last_seen_write_clock = None
+        self.last_n_samples = None
+
+        self.client_address = None  # Last IP we saw them from
+        self.client_telemetry = {}  # unstructured info from client
 
         self.mark_sent()
 
@@ -306,8 +313,24 @@ class User:
         for key in self.list_keys:
             self.to_send[key] = []
 
+    def update_client_telemetry(self, nct):
+        if not isinstance(nct, dict):
+            raise Exception("New client telemetry not a dict:", nct)
+        merge_into_dict(self.client_telemetry, nct)
+
+def merge_into_dict(a, b):
+    for k in b:
+        if k not in a:
+            a[k] = b[k]
+        elif isinstance(a[k], list) and isinstance(b[k], list):
+            a[k] += b[k]
+        elif isinstance(a[k], dict) and isinstance(b[k], dict):
+            merge_into_dict(a[k], b[k])
+        else:
+            a[k] = b[k]
+
 users: Dict[str, Any] = {} # userid -> User
-def sendall(key, value, exclude=[]):
+def sendall(key, value, exclude=None):
     for userid, user in users.items():
         if userid not in exclude:
             user.send(key, value)
@@ -538,13 +561,58 @@ def fix_volume(data, backing_data, n_people):
     data *= state.global_volume
     return data
 
+def get_telemetry():
+    clients = {}
+    for userid, user in users.items():
+        c = {}
+        raw = copy.deepcopy(user.__dict__)
+        del raw["list_keys"]  # redundant
+
+        try:
+            c["client_time_to_next_client_samples"] = raw["last_heard_server_clock"] - raw["last_seen_write_clock"] - raw["client_telemetry"]["audio_offset"] + raw["last_n_samples"]
+            c["client_time_to_next_client_seconds"] = c["client_time_to_next_client_samples"] / SAMPLE_RATE
+        except:
+            pass
+
+        c["raw"] = raw
+        clients[userid] = c
+
+    now = time.time()
+    result = {
+        "request_time": now,
+        "server": {
+            "server_startup_time": SERVER_STARTUP_TIME,
+            "server_uptime": int(now) - SERVER_STARTUP_TIME,
+            "server_version": SERVER_VERSION,
+            "server_branch": SERVER_BRANCH,
+            "server_clock": calculate_server_clock(),
+            "server_sample_rate": SAMPLE_RATE,
+            "n_connected_users": len(users),
+            "queue_size": QUEUE_LENGTH / FRAME_SIZE, # in 128-sample frames
+            "events": get_events_to_send(),
+            "state": copy.deepcopy(state.__dict__),  # XXX: refine this / dedupe
+        },
+        "clients": clients
+        # XXX: missing client IPs, what else
+    }
+    del result["server"]["state"]["backing_track"]  # XXX: ok but we really shouldn't have copied it in the first place
+    return result
+
 def handle_json_post(in_json_raw, in_data):
     in_json = json.loads(in_json_raw)
 
+    if in_json.get("request", None):
+        if in_json["request"] == "get_telemetry":
+            result = get_telemetry()
+            return json.dumps(result), np.zeros(0)
+        else:
+            return json.dumps({"error": "unknown request " + in_json["request"]}), np.zeros(0)
+
     query_string = in_json["query_string"]
+    client_address = in_json.get("client_address", None)
 
     out_data, x_audio_metadata = handle_post(
-        in_data, query_string, print_status=True)
+        in_data, query_string, print_status=True, client_address=client_address)
 
     return json.dumps({
         "x-audio-metadata": x_audio_metadata,
@@ -570,7 +638,10 @@ def handle_special(query_params, server_clock, user=None, client_read_clock=None
     msg_chats = query_params.get("chat", None)
     if msg_chats:
         for msg_chat in json.loads(msg_chats):
-            sendall("chats", (username, msg_chat), exclude=[userid])
+            if user is not None:
+                sendall("chats", (user.name, msg_chat), exclude=[user.userid])
+            else:
+                sendall("chats", ("[ANNOUNCEMENT]", msg_chat))
 
     bpm = query_params.get("bpm", None)
     if bpm:
@@ -679,7 +750,7 @@ def extract_params(params, keys):
 def get_events_to_send() -> Any:
     return [{"evid": i[0], "clock": i[1]} for i in events.items()]
 
-def handle_post(in_data, query_string, print_status) -> Tuple[Any, str]:
+def handle_post(in_data, query_string, print_status, client_address=None) -> Tuple[Any, str]:
     in_data = in_data.view(dtype=np.float32)
 
     raw_params = {}
@@ -742,6 +813,13 @@ def handle_post(in_data, query_string, print_status) -> Tuple[Any, str]:
     update_users(userid, username, server_clock, client_read_clock)
     user = users[userid]
 
+    if client_address is not None:
+        user.client_address = client_address
+
+    client_telemetry = query_params.get("client_telemetry", None)
+    if client_telemetry:
+        user.update_client_telemetry(json.loads(client_telemetry))
+
     rms_volume = query_params.get("rms_volume", None)
     if rms_volume:
         user.rms_volume = float(rms_volume)
@@ -803,6 +881,7 @@ def handle_post(in_data, query_string, print_status) -> Tuple[Any, str]:
     state.last_request_clock = server_clock
 
     n_samples = len(in_data)
+    user.last_n_samples = n_samples
 
     if client_write_clock is None:
         pass

html/app.js
@@ -702,11 +702,12 @@ export class SingerClient extends EventTarget {
 
     check(context, "Cannot construct SingerClient without a valid context! Context is:", context);
 
-    this.audio_vol_adjustment_ = 1;
-    this.speakerMuted_ = speakerMuted;
-    this.micMuted_ = micMuted;
-
     this.ctx = context;
+    this.metadata_send_queue = [];
+    this.telemetry_send_queue = [];
+
+    this.audio_vol_adjustment_ = 1;
+
     this.offset = offset;
     this.secretId = secretId;
     this.username = username;
@@ -725,16 +726,12 @@ export class SingerClient extends EventTarget {
     this.alarms_fired = {};
     this.cur_clock_cbs = [];
 
-    this.metadata_send_queue = [];
     this.events_seen_already = {};
 
     // This is a bit of a hack, but whatever.
     this.backing_track_start_clock = null;
 
     this.handle_message_bound = this.handle_message.bind(this);
-
-    this.ctx.set_mic_pause_mode(this.micMuted_);
-    this.ctx.set_speaker_pause_mode(this.speakerMuted_);
 
     // Hack on the new CustomEvent-based mark stuff into the old event_hooks system
     this.event_hooks.push((data) => {
@@ -747,6 +744,10 @@ export class SingerClient extends EventTarget {
     })
 
     this.connect_();
+
+    this.speakerMuted = speakerMuted;
+    this.micMuted = micMuted;
+    this.send_telemetry("user_agent", navigator.userAgent);  // XXX: this is stupid, do this on the server
   }
 
   declare_event(evid, offset) {
@@ -804,6 +805,12 @@ export class SingerClient extends EventTarget {
         }
         this.metadata_send_queue = [];
       }
+      if (this.telemetry_send_queue.length != 0) {
+        for (const [key, value, append] of this.telemetry_send_queue) {
+          this.connection.send_telemetry(key, value, append);
+        }
+        this.telemetry_send_queue = [];
+      }
       this.dispatchEvent(new Event("connectivityChange"));
     }, err => {
       this.close();
@@ -821,11 +828,13 @@ export class SingerClient extends EventTarget {
   set speakerMuted(mode) {
     this.speakerMuted_ = mode;
     this.ctx.set_speaker_pause_mode(mode);
+    this.send_telemetry("speaker_muted", mode);
   }
 
   set micMuted(mode) {
     this.micMuted_ = mode;
     this.ctx.set_mic_pause_mode(mode);
+    this.send_telemetry("mic_muted", mode);
   }
 
   close() {
@@ -853,6 +862,14 @@ export class SingerClient extends EventTarget {
     } else {
       this.metadata_send_queue.push([key, value, append]);
       console.warn("Can't send metadata yet; buffering until connected, queue is:", this.metadata_send_queue);
+    }
+  }
+
+  send_telemetry(key, value, append) {
+    if (this.connection && this.hasConnectivity) {
+      this.connection.send_telemetry(key, value, append);
+    } else {
+      this.telemetry_send_queue.push([key, value, append]);
     }
   }
 
@@ -887,7 +904,9 @@ export class SingerClient extends EventTarget {
     }
     this.mic_buf.push(chunk);
 
-    this.diagnostics.webAudioJankCurrent = msg.jank;
+    this.diagnostics.web_audio_jank = msg.jank / 1000.0;  // convert from ms to s
+    this.diagnostics.web_audio_jank_frac = msg.jank / msg.jank_over;  // fraction of dropped callbacks
+    this.diagnostics.dropped_calls = msg.dropped_calls;
     // XXX: just for debugging
     // XXX: window.msWebAudioJankCurrent.value = Math.round(msg.jank) + "ms";
 
@@ -1047,6 +1066,10 @@ export class SingerClient extends EventTarget {
       // There are easier ways to compute this, but this works.
       this.diagnostics.client_time_to_next_client = this.diagnostics.client_total_time + this.diagnostics.client_read_slippage;
     }
+    this.send_telemetry("diagnostics", this.diagnostics);  // A bit redundant since we're telling the server things it should already know...
+    if (this.connection?.server_connection?.audio_offset) {
+      this.send_telemetry("audio_offset", this.connection?.server_connection?.audio_offset);  // This is a very weird place to send this
+    }
     this.dispatchEvent(new Event("diagnosticChange"));
   }
 }
@@ -1120,6 +1143,25 @@ export class SingerClientConnection {
     } else {
       this.metadata_to_send[key] = value;
     }
+  }
+
+  send_telemetry(key, value, append) {
+    var telemetry;
+    if ("client_telemetry" in this.metadata_to_send) {
+      telemetry = this.metadata_to_send.client_telemetry;
+    } else {
+      telemetry = {};
+    }
+
+    if (append) {
+      if (!(key in telemetry)) {
+        telemetry[key] = []
+      }
+      telemetry[key].push(value);
+    } else {
+      telemetry[key] = value;
+    }
+    this.metadata_to_send.client_telemetry = telemetry;
   }
 
   server_response(response) {

html/audio-worklet.js
@@ -718,6 +718,8 @@ class Player extends AudioWorkletProcessor {
       this.port.postMessage({
         epoch: this.epoch,
         jank: this.acc_err,
+        jank_over: Date.now() - this.acc_err_since,
+        dropped_calls: this.dropped_calls,
         type: "samples_out",
         chunk: mic_chunk,
       }); // XXX don't transfer , [mic_chunk.data.buffer]);
@@ -727,18 +729,24 @@ class Player extends AudioWorkletProcessor {
 
   profile_web_audio() {
     var now_ms = Date.now();
+    this.calls += 1;
     const process_history_len = 100;
     if (this.process_history_ms === undefined) {
       this.bad_sample_rate = 0;
       this.acc_err = 0;
+      this.acc_err_since = now_ms;
+      this.calls = 0;
       this.process_history_ms = new Array(process_history_len).fill(NaN);
     } else if (!isNaN(this.process_history_ms[0])) {
       var interval = now_ms - this.process_history_ms[0];
+      var total_interval = now_ms - this.acc_err_since;
       var target_interval = process_history_len * 128 * 1000 / sampleRate;
       var err = interval - target_interval;
       var eff_rate = process_history_len * 128 * 1000 / interval;
       this.acc_err += err / process_history_len;
-      log_every(500, "profile_web_audio", sampleRate, eff_rate, this.process_history_ms[0], now_ms, interval, target_interval, err, this.acc_err, this.acc_err / (128 * 1000 / 22050 /* XXX... */));
+      var target_calls = total_interval * sampleRate / 1000 / 128;
+      this.dropped_calls = target_calls - this.calls
+      log_every(500, "profile_web_audio", total_interval, target_calls, this.calls, this.dropped_calls, sampleRate, eff_rate, this.process_history_ms[0], now_ms, interval, target_interval, err, this.acc_err, this.acc_err / (128 * 1000 / 22050 /* XXX... */));
 
       // other parameters of interesst
       // XXX // console.debug("VERYSPAM", currentTime, currentFrame, /* getOutputTimestamp(), performanceTime, contextTime*/);
@@ -751,11 +759,11 @@ class Player extends AudioWorkletProcessor {
         if (this.bad_sample_rate > 2000) {
           console.warn("SAMPLE RATE STILL BAD. Should be", sampleRate, "but seeing", eff_rate, ". Restarting app.");
           // Ask the main app to reload the audio input device
-          this.killed = true;
+          /* XXX this.killed = true;
           throw {
             message: "Your computer's audio system is lagging a lot, which is breaking the app. Please disconnect any bluetooth headphones or speakers, close unnecessary apps / reduce load on your computer, then refresh the page and try again.",
             unpreventable: true,
-          };
+          }; */
         }
       }
     }
@@ -811,6 +819,14 @@ class Player extends AudioWorkletProcessor {
           output = new Float32Array(output.length);
         }
         this.process_normal(input, output);
+        // Hack: If we've fallen behind, pretend we were called some extra times to skip a bit of audio until we catch up. This will audibly glitch (but there is an extremely high likelihood that we actually just did anyway, to get here.)
+        while (this.dropped_calls > 15 /* arbitrary */) {
+          //XXX : we got to -348 dropped calls, whoops, which gets us negative 1s read slippage oops
+          console.warn("Making up for lost time by throwing away some audio...");
+          this.calls += 1;
+          this.dropped_calls -= 1;
+          this.process_normal(input, output);
+        }
       }
       // Handle stereo output by cloning mono output.
       for (var chan = 1; chan < outputs[0].length; chan++) {

html/net.js
@@ -368,7 +368,7 @@ export async function samples_to_server(outdata, target_url, send_metadata) {
     const skip_params = []
     const truthy_params = ['track', 'monitor'];
     const nonnull_params = ['write_clock', 'volume', 'backing_volume', 'bpm', 'repeats', 'bpr'];
-    const stringify_params = ['chat', 'mic_volume', 'event_data'];
+    const stringify_params = ['chat', 'mic_volume', 'event_data', 'client_telemetry'];
     const flag_params = ['request_lead', 'mark_start_singing', 'mark_stop_singing'];
 
     for (var k in send_metadata) {

server_wrapper.py
@@ -113,7 +113,7 @@ def handle_post_special(query_string, print_status=True):
     return data.tobytes(), x_audio_metadata
 
 def handle_post(userid, n_samples, in_data_raw,
-                query_string, print_status=True) -> Tuple[Any, str]:
+                query_string, print_status=True, client_address=None) -> Tuple[Any, str]:
     if not userid.isdigit():
         raise ValueError("UserID must be numeric; got: %r"%userid)
     try:
@@ -152,12 +152,12 @@ def handle_post(userid, n_samples, in_data_raw,
     query_string += '&rms_volume=%s'%rms_volume
 
     data, x_audio_metadata = handle_json_post(
-        in_data, query_string, print_status)
+        in_data, query_string, print_status, client_address=client_address)
 
     # Divide data into user_summary and raw audio data
     n_users_in_summary, = struct.unpack(">H", data[:2])
     user_summary_n_bytes = server.summary_length(n_users_in_summary)
-    
+
     user_summary = data[:user_summary_n_bytes]
     raw_audio = data[user_summary_n_bytes:].view(np.float32)
 
@@ -178,6 +178,38 @@ def handle_post(userid, n_samples, in_data_raw,
             -1 if client_no_data else rms_volume))
 
     return data.tobytes(), x_audio_metadata
+
+def handle_json_post(in_data, query_string, print_status, client_address=None):
+    in_json = {
+        "query_string": query_string,
+        "print_status": print_status,
+        "client_address": client_address,
+    }
+    out_json_raw, out_data = backend.handle_post(json.dumps(in_json), in_data)
+
+    out_json = json.loads(out_json_raw)
+
+    if "error" in out_json:
+        inner_bt = ""
+        if "inner_bt" in out_json:
+            inner_bt = "\nBackend error details: " + out_json["inner_bt"]
+        raise Exception(out_json["error"] + inner_bt)
+
+    return out_data, out_json["x-audio-metadata"]
+
+def get_telemetry(start_response) -> None:
+    in_json = {
+        "request": "get_telemetry"
+    }
+    out_json_raw, _ = backend.handle_post(json.dumps(in_json), np.zeros(0))
+    start_response(
+        '200 OK',
+        [("Access-Control-Allow-Origin", "*"),
+         ("Access-Control-Max-Age", "86400"),
+         ("Access-Control-Expose-Headers", "X-Audio-Metadata"),
+         ("Content-Type", "application/json")])
+    return out_json_raw.encode("utf-8"),
+
 
 def do_OPTIONS(environ, start_response) -> None:
     start_response(
@@ -209,6 +241,9 @@ def do_GET(environ, start_response) -> None:
         ps.print_stats()
         start_response('200 OK', [])
         return s.getvalue().encode("utf-8"),
+
+    if environ.get('PATH_INFO', '') == "/api/telemetry":
+        return get_telemetry(start_response)
 
     server_clock = server.calculate_server_clock()
 
@@ -255,6 +290,12 @@ def do_POST(environ, start_response) -> None:
 
     query_string = environ['QUERY_STRING']
 
+    try:
+        client_address = environ['HTTP_X_FORWARDED_FOR']
+    except KeyError:
+        client_address = environ.get('REMOTE_ADDR', "")
+    # XXX: do something with it
+
     # For some reason parse_qs can't handle an empty query string
     if len(query_string) > 0:
         query_params = urllib.parse.parse_qs(query_string, strict_parsing=True)
@@ -276,7 +317,7 @@ def do_POST(environ, start_response) -> None:
             del users[userid]
 
         if userid is not None:
-            data, x_audio_metadata = handle_post(userid, n_samples, in_data_raw, query_string)
+            data, x_audio_metadata = handle_post(userid, n_samples, in_data_raw, query_string, client_address=client_address)
         else:
             data, x_audio_metadata = handle_post_special(query_string)
     except Exception as e:

shm.py
@@ -41,7 +41,7 @@ def server_turn(buf):
 
 def encode_json_and_data(buf, json_raw, data, throw_exceptions):
     data = data.view(dtype=np.uint8)
-    
+
     index = 1
 
     json_raw_bytes = json_raw.encode("utf-8")

commit 955a6d8d4e78c909857e824153684dd3c4013060

Fix bug introduced in 6cca76ca where sendall only worked with exclude set

server.py
@@ -332,7 +332,7 @@ def merge_into_dict(a, b):
 users: Dict[str, Any] = {} # userid -> User
 def sendall(key, value, exclude=None):
     for userid, user in users.items():
-        if userid not in exclude:
+        if not exclude or userid not in exclude:
             user.send(key, value)
 
 def wrap_get(queue, start, len_vals) -> Any: